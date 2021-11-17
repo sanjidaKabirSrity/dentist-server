23779bb7f35d364ac6cf5b15b2900e96642681ad
sanjidaKabirSrity <sanjida.kabir.srity@gmail.com>
2021-11-18 00:13:48 +0600

    post

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+const cors = require("cors");
 const { MongoClient } = require('mongodb');
 require("dotenv").config();
 const ObjectId = require("mongodb").ObjectId;
@@ -10,8 +10,25 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${DB_USER}:${DB_PASS}@cluster0.bonkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bonkw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run(){
+    try{
+        await client.connect();
+        const database = client.db('doctors_portal');
+        const appointmentsCollection = database.collection('appointments');
+
+        app.post('/appointments', async (req , res) => {
+            // m 
+        })
+    }
+    finally{
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/' , (req , res) => {
     res.send('Running Dentist Crud Server')
